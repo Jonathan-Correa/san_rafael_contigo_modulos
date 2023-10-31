ec60ff26d8860f38b99f3592944ff0b472f42ede
Clínica San Rafael <jefedesarrollodesoftware@socimedicos.com>
2023-10-31 16:54:53 -0500

    Cambio de nombre de paquete

--- test/san_rafael_contigo_modulos_test.dart
+++ test/san_rafael_contigo_modulos_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:san_rafael_contigo_modulos/san_rafael_contigo_modulos.dart';
+import 'package:csr_modules/san_rafael_contigo_modulos.dart';
 
 void main() {
   test('adds one to input values', () {
